d50232923a63351a9f50496e0a1722b05bb11c41
Mirrorlock <mar.penev@gmail.com>
2019-03-20 17:02:44 +0200

    Initial commit working on users authentication

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,21 @@
+class SessionController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by username: params[:username] 
+    puts(user.username);
+    if(user && user.authenticate(params[:password]))
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Successfully logged in!"
+    else
+      flash.now[:alert] = "Wrong username or password!"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Successfully logged out!"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'login', to: 'session#new', as: "login" 
+  post 'login', to: 'session#create'
+  get 'logout', to: 'session#destroy', as: 'logout' 
+  resources :users
+  root 'users#index', as: 'root'
 end
