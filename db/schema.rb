84a6f3c96975d060768bdf04614cea433a829c75
Mirrorlock <mar.penev@gmail.com>
2019-04-04 00:31:08 +0300

    Adding info.txt - useful information is to be stored there.

--- db/schema.rb
+++ db/schema.rb
@@ -38,18 +38,15 @@ ActiveRecord::Schema.define(version: 2019_04_03_102107) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
-    t.string "price"
-    t.string "double"
-    t.string "shopping_list_id"
-    t.string "integer"
+    t.decimal "price", precision: 10, scale: 2
+    t.integer "shopping_list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "shopping_lists", force: :cascade do |t|
     t.string "name"
-    t.string "family_id"
-    t.string "integer"
+    t.integer "family_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
